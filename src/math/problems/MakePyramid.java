f1ce4c5a12b495ea3c7077a7df1784996f5e9bec
[rubayethasan38] <[rubayet.hasan@outlook.com]>
2018-09-10 16:49:39 -0400

    eight-commit

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -1,50 +1,39 @@
 package math.problems;
 
-public class MakePyramid {
-    public static void main(String[] args) {
-
-        //   Implement a large Pyramid of stars in the screen with java.
-        //
-        //                    *
-        //                   * *
-        //                  * * *
-        //                 * * * *
-        //                * * * * *
-        //               * * * * * *
-
-        //Start Here
-        int n = 10;
-        printTriagle(n);
-    }
-
-    // Function to demonstrate printing pattern
-    public static void printTriagle(int n) {
-        // number of spaces
-        int k = 2 * n - 2;
-
-        // outer loop to handle number of rows
-        //  n in this case
-        for (int i = 0; i < n; i++) {
-
-            // inner loop to handle number spaces
-            // values changing acc. to requirement
-            for (int j = 0; j < k; j++) {
-                // printing spaces
+public class MakePyramid
+{
+    public static void printTriangle ( int n)
+    {
+        int k = 4 * n -4;
+        for (int i=0; i<n; i++)
+        {
+            for (int j = 0; j<k; j++)
+            {
                 System.out.print(" ");
+
             }
-
-            // decrementing k after each loop
-            k = k - 1;
-
-            //  inner loop to handle number of columns
-            //  values changing acc. to outer loop
-            for (int j = 0; j <= i; j++) {
-                // printing stars
-                System.out.print("* ");
+            k = k-1;
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print(" *");
             }
-
-            // ending line after each row
             System.out.println();
         }
+
+    }
+    public static void main(String[] args)
+    {
+
+        /*   Implement a large Pyramid of stars in the screen with java.
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * * * *
+         */
+        int n = 4;
+        printTriangle(n);
+
     }
 }
--- src/math/problems/Pattern.java
+++ src/math/problems/Pattern.java
@@ -1,15 +1,40 @@
 package math.problems;
+import databases.ConnectToSqlDB;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pattern {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		/* Read this numbers, find the pattern then implement the logic from this pattern.which will give you this output.
 		 * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32............
 		 *
+		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 *
 		 */
+		int count = 1, j = 0;
+		int num = 100;
+		LowestNumber.ConnectToSqlDB connectDB = new LowestNumber.ConnectToSqlDB();
+		List<String> storePattern = new ArrayList<String>();
+		int[] array = new int[40];
 
+		while (num > 0) {
+			for (int i = 0; i < 10; i++) {
+				System.out.print(" " + num);
+				array[j] = num;
+				num -= count;
+				j++;
 
-
+			}
+			int i = count++;
+		}
+		System.out.println(" ");
+		ConnectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_pattern", "column_pattern");
+		storePattern = connectDB.readDataBase("tbl_pattern", "column_pattern");
+		System.out.println("Data is reading from the Table (tbl_pattern) and displaying to the console");
+		for (String st : storePattern) {
+			System.out.print(" " + st);
+		}
 	}
 }
